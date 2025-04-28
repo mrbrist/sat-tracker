54455e09421d44da57135cbfb441c17b84a3d18b
Joshua Weston <bristol2801@gmail.com>
2025-04-28 19:21:42 +0100

    Updates

---
+++ precache-manifest.603e03490b768c7351ed25e916e9aff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4964d24edd271b81a8ca1ff0877f74d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7461bc712335ee178b1c",
+    "url": "./static/css/main.3727a7b1.chunk.css"
+  },
+  {
+    "revision": "53ba3f7b161f4272e088",
+    "url": "./static/js/2.beacf1cd.chunk.js"
+  },
+  {
+    "revision": "e63a02c5c9fb17cf9fa8",
+    "url": "./static/js/3.c2b35cdc.chunk.js"
+  },
+  {
+    "revision": "7461bc712335ee178b1c",
+    "url": "./static/js/main.33cc6d04.chunk.js"
+  },
+  {
+    "revision": "97ffe34cca694535eb0b",
+    "url": "./static/js/runtime~main.5f967d81.js"
+  }
+]);
